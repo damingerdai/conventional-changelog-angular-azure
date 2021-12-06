e24b27741fac2b9a13cf6dba847745035f8f00ce
Arthur Ming <guobin.ming@pwc.com>
2021-12-06 15:55:21 +0800

    chore: remove useless commtent

--- src/parser-opts.ts
+++ src/parser-opts.ts
@@ -2,11 +2,9 @@ import { COMMIT_FORMAT_PREFIX, ParserOptions } from './types';
 
 export const parserOpts: Partial<ParserOptions> = {
     headerCorrespondence: ['azure', 'type', 'scope', 'message'],
-    // Keep in sync with checkCommitFormat
     headerPattern: new RegExp(`^(Merged? PR \\d+: )?${COMMIT_FORMAT_PREFIX.source} (.*)$`, 'u'),
     mergeCorrespondence: ['id', 'source'],
     mergePattern: /^Merged PR (\\d+)/u,
-
     noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'Note'],
     revertCorrespondence: ['header'],
     revertPattern: /^Revert\s"(.*)"?/u,
